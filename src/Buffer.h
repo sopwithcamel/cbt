a0c38117c3a957b2ee7d099de0078dc6800e7c8d
Hrishikesh Amur <amur@gatech.edu>
2012-10-02 13:22:56 -0400

    fixed cpplint errors for Buffer.h

--- src/Buffer.h
+++ src/Buffer.h
@@ -1,5 +1,28 @@
-#ifndef LIB_COMPRESS_BUFFER_H
-#define LIB_COMPRESS_BUFFER_H
+// Copyright (C) 2012 Georgia Institute of Technology
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+// ---
+// Author: Hrishikesh Amur
+
+#ifndef SRC_BUFFER_H_
+#define SRC_BUFFER_H_
 #include <stdint.h>
 #include <stdio.h>
 #include <vector>
@@ -59,7 +82,7 @@ namespace cbt {
           // clears all buffer state
           ~Buffer();
           // add a list and allocate memory
-          List* addList(bool isLarge=false);
+          List* addList(bool isLarge = false);
           void addList(List* l);
           /* clear the lists_ vector. This does not free space allocated
            * for the buffers but merely deletes the pointers. To avoid
@@ -126,8 +149,7 @@ namespace cbt {
           PageAction pageAct_;
           pthread_cond_t pageCond_;
           pthread_mutex_t pageMutex_;
-#endif //ENABLE_PAGING
+#endif  // ENABLE_PAGING
     };
 }
-
-#endif
+#endif  // SRC_BUFFER_H_
