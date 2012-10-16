3fecbff0400c3e6d72aac79bac66d5fef4b08aa3
Hrishikesh Amur <amur@gatech.edu>
2012-10-16 02:51:42 -0400

    Extra header necessary

---
+++ src/ProtobufPartialAgg.h
@@ -0,0 +1,23 @@
+#ifndef PROTOBUF_PARTIALAGG_H
+#define PROTOBUF_PARTIALAGG_H
+#include <google/protobuf/io/coded_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+#include "PartialAgg.h"
+
+class Token;
+class ProtobufOperations : public Operations {
+  public:
+	ProtobufOperations() {}
+	virtual ~ProtobufOperations() {}
+
+    SerializationMethod getSerializationMethod() const { return PROTOBUF; }
+
+	/* serialize into file */
+	virtual bool serialize(PartialAgg* p,
+            google::protobuf::io::CodedOutputStream* output) const = 0;
+	/* deserialize from file */
+	virtual bool deserialize(PartialAgg* p,
+            google::protobuf::io::CodedInputStream* input) const = 0;
+};
+
+#endif
